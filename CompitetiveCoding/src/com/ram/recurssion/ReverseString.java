e85407339d42c77dc47a1d371dff696e65de529e
siva-960 <sivaramireddy960@gmail.com>
2023-02-05 09:24:00 +0530

    my first commint in this project

---
+++ CompitetiveCoding/src/com/ram/recurssion/ReverseString.java
@@ -0,0 +1,32 @@
+package com.ram.recurssion;
+
+import java.util.Scanner;
+
+public class ReverseString {
+	
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		String st=new String("ram");
+		st.concat("kittu");//eligible garbage collection since String object is immutable if u made any changes it will create new object but here created object is 
+		//doesn't referenced by any variable so it is removed by garbage collecter.
+		System.out.println(st);
+		StringBuffer sb=new StringBuffer("pravee");
+		sb.append("macha");
+		System.out.println(sb);//since sb object is mutable if we made any changes it will make that changes in existing object only but not creates any new objects
+		
+		//Case:2
+		
+		String s1=new String("ram");
+		String s2=new String("ram");
+		System.out.println(s1==s2);
+		System.out.println(s1.equals(s2));
+		
+		StringBuffer s3=new StringBuffer("ram");
+		StringBuffer s4=new StringBuffer("ram");
+		System.out.println(s3==s4);
+		System.out.println(s3.equals(s4));
+		
+				
+	}
+}
